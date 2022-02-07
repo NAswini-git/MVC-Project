87268c3f0899d784835388d65a725b95aaa2bee2
NAswini-git <98838060+NAswini-git@users.noreply.github.com>
2022-02-07 12:41:27 +0530

    Update InsertProducts.java

--- com/shopping/view/InsertProducts.java
+++ com/shopping/view/InsertProducts.java
@@ -4,30 +4,30 @@ import com.shopping.dao.Products;
 
 /**
  * It is meant to receive input from user to insert.
- * @author AswiniN
  *
+ * @author AswiniN
  */
 public class InsertProducts {
 
-	/**
-	 * It gets product details.
-	 * @return
-	 */
-	public static Products getProductDetails() {
-		final Products products = new Products();
+    /**
+     * It gets product details.
+     * @return
+     */
+    public static Products getProductDetails() {
+	final Products products = new Products();
 		
-		System.out.println("Enter Product Name:");
-		products.setProductName(LoginPortal.SCANNER.next());
-		System.out.println("Enter Brand Name:");
-		products.setBrandName(LoginPortal.SCANNER.next());
-		System.out.println("Enter Price:");
-		products.setPrice(LoginPortal.SCANNER.next());
-		System.out.println("Enter Size:");
-		products.setSize(LoginPortal.SCANNER.next());
-		System.out.println("Enter User:");
-		products.setUser(LoginPortal.SCANNER.next());
-		System.out.println("Enter Quantity:");
-		products.setQuantity(LoginPortal.SCANNER.nextInt());
-		return products;	
-	}
+	System.out.println("Enter Product Name:");
+	products.setProductName(LoginPortal.SCANNER.next());
+	System.out.println("Enter Brand Name:");
+	products.setBrandName(LoginPortal.SCANNER.next());
+	System.out.println("Enter Price:");
+	products.setPrice(LoginPortal.SCANNER.next());
+	System.out.println("Enter Size:");
+	products.setSize(LoginPortal.SCANNER.next());
+	System.out.println("Enter User:");
+	products.setUser(LoginPortal.SCANNER.next());
+	System.out.println("Enter Quantity:");
+	products.setQuantity(LoginPortal.SCANNER.nextInt());
+	return products;	
+    }
 }
