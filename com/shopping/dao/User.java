864d2fbd1047ef52238b54a70326f862d2279148
NAswini-git <98838060+NAswini-git@users.noreply.github.com>
2022-02-07 10:58:49 +0530

    Update User.java

--- com/shopping/dao/User.java
+++ com/shopping/dao/User.java
@@ -6,49 +6,49 @@ package com.shopping.dao;
  *
  */
 public class User {
-	private int preferredChoice;
-	private int typeOfUser;
-	private String emailId;
-	private String password;
-	private String userType;
+    private int preferredChoice;
+    private int typeOfUser;
+    private String emailId;
+    private String password;
+    private String userType;
 	
-	public String getUserType() {
-		return userType;
-	}
+    public String getUserType() {
+	return userType;
+    }
 
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
+    public void setUserType(String userType) {
+	this.userType = userType;
+    }
 
-	public int getPreferredChoice() {
-		return preferredChoice;
-	}
+    public int getPreferredChoice() {
+	return preferredChoice;
+    }
 
-	public void setPreferredChoice(int preferredChoice) {
-		this.preferredChoice = preferredChoice;
-	}
+    public void setPreferredChoice(int preferredChoice) {
+	this.preferredChoice = preferredChoice;
+    }
 
-	public int getTypeOfUser() {
-		return typeOfUser;
-	}
+    public int getTypeOfUser() {
+	return typeOfUser;
+    }
 
-	public void setTypeOfUser(int typeOfUser) {
-	    this.typeOfUser = typeOfUser;
-	}
+    public void setTypeOfUser(int typeOfUser) {
+        this.typeOfUser = typeOfUser;
+    }
 	
-	public String getEmailId() {
-		return emailId;
-	}
+    public String getEmailId() {
+	return emailId;
+    }
 	
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
+    public void setEmailId(String emailId) {
+	this.emailId = emailId;
+    }
 	
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+	return password;
+    }
 	
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+	this.password = password;
+    }
 }
