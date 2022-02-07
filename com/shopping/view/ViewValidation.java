ebe076b90d82e7d09ca551f581ea94abffe34b0c
NAswini-git <98838060+NAswini-git@users.noreply.github.com>
2022-02-07 13:04:11 +0530

    Update ViewValidation.java

--- com/shopping/view/ViewValidation.java
+++ com/shopping/view/ViewValidation.java
@@ -8,36 +8,36 @@ import com.shopping.dao.User;
  *
  */
 public class ViewValidation {
-	final static User user = new User();
-	/**
-	 * It validates EmailId.
-	 * @param emailId
-	 * @return
-	 */
-	public static String validateEmailId(String emailId) {
+    final static User user = new User();
+    /**
+     * It validates EmailId.
+     * @param emailId
+     * @return
+     */
+    public static String validateEmailId(String emailId) {
 			
-		if (emailId != null && emailId.matches("^[A-Za-z0-9]{4,}+@[a-z]+.[a-z]{2,3}$")) { 
-			 return emailId;
-		} else {
-			System.out.println("Invalid Email Id.");
-		    LoginPortal.getEmailId();
-		    return null;
-	    }	
-	}
+	if (emailId != null && emailId.matches("^[A-Za-z0-9]{4,}+@[a-z]+.[a-z]{2,3}$")) { 
+	    return emailId;
+	} else {
+	    System.out.println("Invalid Email Id.");
+	    LoginPortal.getEmailId();
+	    return null;
+	}	
+    }
 	
-	/**
-	 * It validates Password.
-	 * @param password
-	 * @return
-	 */
-	public static String validatePassword(String password) {
+    /**
+     * It validates Password.
+     * @param password
+     * @return
+     */
+    public static String validatePassword(String password) {
 	
-		if (password.matches("^(.+)(?=.*[@#$%^&-+=()])(?=\\S+$).{6,10}$"))	{
-		   System.out.println("Valid Password.");	
-		} else {
-			System.out.println("Invalid");
-			password = LoginPortal.getPassword();
-		}
-		return password;
+	if (password.matches("^(.+)(?=.*[@#$%^&-+=()])(?=\\S+$).{6,10}$"))	{
+	    System.out.println("Valid Password.");	
+	} else {
+	    System.out.println("Invalid");
+	    password = LoginPortal.getPassword();
 	}
+	return password;
+    }
 }
