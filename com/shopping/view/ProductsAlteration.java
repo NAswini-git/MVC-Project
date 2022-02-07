c4a63344de078280a28b350d2815ea834621f787
NAswini-git <98838060+NAswini-git@users.noreply.github.com>
2022-02-07 12:59:54 +0530

    Update ProductsAlteration.java

--- com/shopping/view/ProductsAlteration.java
+++ com/shopping/view/ProductsAlteration.java
@@ -11,68 +11,68 @@ import com.shopping.dao.Products;
  *
  */
 public class ProductsAlteration {
-	 static Products products = new Products();
-	 final static ProductsController productController = new ProductsController();
+    static Products products = new Products();
+    final static ProductsController productController = new ProductsController();
 	
-	 /**
-	 * To delete a product.
-	 * @return
-	 */
-	public static Products getDeleteDetails() {	
-		System.out.println("Enter Product Name:");
-		products.setProductName(LoginPortal.SCANNER.next());
-		System.out.println("Enter Brand Name:");
-		products.setBrandName(LoginPortal.SCANNER.next());
-		return products;
-	}
+    /**
+     * To delete a product.
+     * @return
+     */
+    public static Products getDeleteDetails() {	
+	System.out.println("Enter Product Name:");
+	products.setProductName(LoginPortal.SCANNER.next());
+	System.out.println("Enter Brand Name:");
+	products.setBrandName(LoginPortal.SCANNER.next());
+	return products;
+    }
 	
-	/**
-	 * To update a product.
-	 * @return
-	 */
-	public static Products getUpdateDetails() {
-		 System.out.println("Enter Product Name:");
-		 products.setProductName(LoginPortal.SCANNER.next());
-		 System.out.println("Update Criteria: 1. Price 2. Quantity");
-		 final int updateChoice = LoginPortal.SCANNER.nextInt();
+    /**
+     * To update a product.
+     * @return
+     */
+     public static Products getUpdateDetails() {
+	 System.out.println("Enter Product Name:");
+	 products.setProductName(LoginPortal.SCANNER.next());
+	 System.out.println("Update Criteria: 1. Price 2. Quantity");
+	 final int updateChoice = LoginPortal.SCANNER.nextInt();
 		 
-		 switch(updateChoice) {
-		 case 1:
-			 products = ProductsAlteration.getPrice();
-			 break;
+	 switch(updateChoice) {
+	 case 1:
+       	     products = ProductsAlteration.getPrice();
+	     break;
 	     
-		 case 2:
-			 products = ProductsAlteration.getQuantity();
-			 break;
-		 }
-		return products;
+	 case 2:
+	     products = ProductsAlteration.getQuantity();
+	     break;
 	}
+	return products;
+    }
     
-	/**
-	 * Get Quantity to update.
-	 * @return
-	 */
-	private static Products getQuantity() {
-		System.out.println("Enter the quantity to be changed");
-		products.setQuantity(LoginPortal.SCANNER.nextInt());
-		return products;
-	}
+    /**
+     * Get Quantity to update.
+     * @return
+     */
+    private static Products getQuantity() {
+	System.out.println("Enter the quantity to be changed");
+	products.setQuantity(LoginPortal.SCANNER.nextInt());
+	return products;
+    }
     
-	/**
-	 * Get Price to update.
-	 * @return
-	 */
-	private static Products getPrice() {
-		System.out.println("Enter Price");
-		products.setPrice(LoginPortal.SCANNER.next());
-		return products;	
-	}
+    /**
+     * Get Price to update.
+     * @return
+     */
+    private static Products getPrice() {
+	System.out.println("Enter Price");
+	products.setPrice(LoginPortal.SCANNER.next());
+	return products;	
+    }
 	
-	/**
-	 *  Show the products available in the database.
-	 */
-	public static void showProductDetails() {
-		List<Products> productsList = productController.showProductDetails();
-		System.out.println(productsList);
-	}
+    /**
+     *  Show the products available in the database.
+     */
+    public static void showProductDetails() {
+	List<Products> productsList = productController.showProductDetails();
+	System.out.println(productsList);
+    }
 }
